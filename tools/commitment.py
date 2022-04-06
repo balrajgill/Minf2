30409d1c3b3835b1840d5d4199bd15b7c1987291
Balraj Gill <45169648+balrajgill@users.noreply.github.com>
2022-04-06 17:06:06 +0100

    06/04

--- tools/commitment.py
+++ tools/commitment.py
@@ -1,36 +1,27 @@
+from msilib.schema import MsiFileHash
 from sys import argv
 from hashlib import sha256
 from random import getrandbits
 import binascii
-
+from web3 import Web3
 def getCommitment(msg):
         key = "{0:0{1}x}".format(getrandbits(256), 64) 
-        com = hashing(key + msg)
-
-
-        return (com,key)
+        com = Web3.solidityKeccak(['string'], [key+msg])
+        
+        return (com.hex(),key)
     
 def verify(msg,commitment,key):   
-    return commitment == hashing(key + msg)
-
-def hashing(value):
-    #print(sha256(value.encode()).hexdigest())
-    return sha256(value.encode()).hexdigest()
+    return commitment == (Web3.solidityKeccak(['string'], [key+msg])).hex()
 
 
 
 
 if __name__ == '__main__':
     commit,key = getCommitment("testing")
-    hex_val = binascii.hexlify(commit.encode())
-    a = int(hex_val,16)
-    b = hex(a)[2:]
-    c = bytes.fromhex(b)
+    key = "7ca51aedc46cfa0470b187d0f8cf0e0eb2450b8672096b30d4d5418bed18ffe7"
+    commit = "0x0aac5107c75ca15e39819b6011a326c3dbb48689a61bf5f95457292e47f0834c"
+    print(verify("testing",commit,key))
     print(commit)
-    print(hex_val)
-    print(a)
-    print(b)
-    print(c.decode())
     
     
     
--- uitest.py
+++ uitest.py
@@ -5,12 +5,15 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.dropdown import DropDown
 from matplotlib.cbook import boxplot_stats
-import vote1
-test_floatie = BoxLayout()
-test_floatie.padding = 30
-test_floatie.spacing = 20
+from kivy.uix.textinput import TextInput
+from matplotlib.pyplot import text
+from numpy import size
+test_floatie = FloatLayout()
+
+
 
 global currentSelectedVote
 currentSelectedVote = "sfsdf"
@@ -20,7 +23,7 @@ def select(drop_button, text, btn):
     currentSelectedVote = text
     print("select "+currentSelectedVote)
     f = open("tools/currentvote.txt", "w")
-    f.write(text[3:])
+    f.write(text)
     f.close()
 
 def chooseVote(drop_button, text, btn):
@@ -28,12 +31,14 @@ def chooseVote(drop_button, text, btn):
     a = f.read()
     f.close()
     print("read = " +a)
-
-dropButton = Button(text="Select Vote", size_hint=(0.7, 0.2))
+    drop_button.text = f"You Selected {a}"
+    drop_button.disabled = True
+    
+dropButton = Button(text="Select Vote", size_hint=(0.25, 0.15),pos_hint={"x":0.07, "top":0.4})
 
 # Create a drop down
 dropdown = DropDown()
-votes = vote1.getVotes()
+votes = ["aa","bb","cc"]
 for index in range(len(votes)):
     # Creating our button
     btn = Button(text=f"{index+1}. {votes[index]} ", size_hint_y=None, height=44)
@@ -46,10 +51,16 @@ dropButton.bind(on_release=dropdown.open)
 
 test_floatie.add_widget(dropButton)
 
-dropButton1 = Button(text="Confirm Vote", size_hint=(0.7, 0.2))
-dropButton2 = Button(text="Extra Stuff", size_hint=(0.7, 0.2))
+dropButton1 = Button(text="Confirm Vote", size_hint=(0.25, 0.15), pos_hint={"x":0.37, "top":0.4})
+dropButton2 = Button(text="Extra Stuff", size_hint=(0.25, 0.15),pos_hint={"x":0.67, "top":0.4})
 
-dropButton1.bind(on_release=partial(chooseVote, dropButton1, ""))
+
+textinput = TextInput(text='Hello world')
+textinput.size_hint = (0.8,0.3)
+textinput.pos_hint = {"x":0.1,"top":0.8}
+test_floatie.add_widget(textinput)
+
+dropButton1.bind(on_release=partial(chooseVote, dropButton, ""))
 test_floatie.add_widget(dropButton1)
 test_floatie.add_widget(dropButton2)
 # Running la app
